2e5f0f0dfd78a46d88c4f9fc8c641f01e8fef1ff
stobar93 <sebastiantobarquintero@gmail.com>
2022-04-15 21:22:59 -0500

    Create EditProfileForm component

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import ProfileView from "src/components/Profile";
 
 const Profile: NextPage = () => {
   return (
     <>
       <h1>Profile</h1>
+      <ProfileView />
     </>
   );
 };
--- src/components/Register/styles.tsx
+++ src/components/Register/styles.tsx
@@ -1,6 +1,11 @@
-import { styled } from "@mui/material";
+import { Button, styled } from "@mui/material";
 
 export const Styledform = styled("form")`
   display: flex;
   flex-direction: column;
+`;
+
+export const StyledButton = styled(Button)`
+  margin: 10px;
+  max-width: 300px;
 `;
