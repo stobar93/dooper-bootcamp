1bbd82dbd67754a43a5bccc4ce9ca5e7f8dfc2ad
stobar93 <sebastiantobarquintero@gmail.com>
2022-04-23 21:58:59 -0500

    Move layout components to the right folder

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../Header";
-import MainContainer from "../MainContainer";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
 import * as Styles from "./styles";
 
 type LayoutProps = {
