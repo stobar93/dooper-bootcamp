1b9d8e77f0a3c6cb209e6986800530e3a02ee939
stobar93 <sebastiantobarquintero@gmail.com>
2022-05-02 15:27:33 -0500

    Products CRUD done

---
+++ src/components/InputComponent/components/BooleanInput/styles.tsx
@@ -0,0 +1,7 @@
+import { FormControlLabel, styled } from "@mui/material";
+
+export const StyledFormControlLabel = styled(FormControlLabel)`
+  max-width: 90%;
+  align-self: center;
+  margin: 10px;
+`;
